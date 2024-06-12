e5054010e4a423afddbeab79cbc2baac33af3be2
Raul <raul.ugia@gmail.com>
2024-06-12 19:22:26 +0200

    sign in with google implemented

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {auth} from '../../../utils/firebase'
-import {signInWithEmailAndPassword} from 'firebase/auth'
+import { auth } from '../../../utils/firebase'
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { Navigate } from "react-router-dom";
 
 const Login = () => {
@@ -9,6 +9,8 @@ const Login = () => {
   const [error, setError] = useState();
   const [user, setUser] = useState()
   const [authenticated, setAuthenticated] = useState(false)
+
+  const provider = new GoogleAuthProvider();
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -26,13 +28,25 @@ const Login = () => {
     }
   }
 
+  const googleSignIn = async () => {
+    signInWithPopup(auth, provider)
+        .then(userCredential => {
+            setUser(userCredential.user)
+            setAuthenticated(true)
+        })
+        .catch(err => {
+            setError(err.message)
+        })
+        console.log("success")
+  }
+
   if(authenticated) {
     return <Navigate to="/home" />
   }
 
   return (
     <div className="flex justify-center items-center h-full bg-slate-50">
-      <div className="border py-4 px-3 rounded-lg w-1/5 shadow-md bg-white">
+      <div className="border py-5 px-3 rounded-lg lg:w-1/4 md:w-1/3 shadow-md bg-white">
         <form action="" className="flex flex-col gap-3">
           <input
             type="email"
@@ -65,6 +79,19 @@ const Login = () => {
             onClick={handleSubmit}
           >
             Sign In
+          </button>
+          <div className="flex justify-around items-center gap-1.5 my-2">
+            <div className="flex-grow border-t"></div>
+            <p className="text-sm text-gray-400">or</p>
+            <div className="flex-grow border-t"></div>
+          </div>
+
+          <button
+            type="button"
+            className="border py-1.5 rounded-md bg-black text-white font-semibold"
+            onClick={googleSignIn}
+          >
+            Sign In with Google
           </button>
         </form>
       </div>
