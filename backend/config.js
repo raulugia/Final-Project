2a2832b55114fd87912da93a9fd2fa254a4542cd
Raul <raul.ugia@gmail.com>
2024-06-14 13:16:19 +0200

    config.js created for redis and cloudinary

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  redisConfig: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+  },
+  cloudinaryConfig: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
+};
---
+++ backend/worker.js
@@ -0,0 +1,5 @@
+const Queue = require("bull")
+const sharp = require("sharp")
+const cloudinary = require("cloudinary").v2
+const { PrismaClient } = require("@prisma/client");
+
