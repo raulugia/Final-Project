3020aa6da6b83440f273dadc99113c4705b988b3
Raul <raul.ugia@gmail.com>
2024-06-15 22:49:32 +0200

    adding comments - firebaseAdmin

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,13 @@
+//import the Firebase Admin SDK
 const admin = require("firebase-admin");
+//import the service account key
 const serviceAccount = require("./config/final-project-d6c13-firebase-adminsdk-67z7e-a668b6241d.json")
 
+//initialize the Firebase Admin SDK
 admin.initializeApp({
+    //set the credentials
     credential: admin.credential.cert(serviceAccount),
 })
 
+//export the initialized admin instance
 module.exports = admin
