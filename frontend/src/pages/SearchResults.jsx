de7937bfbf06642bd0a16b496ac5e22e83718460
Raul <raul.ugia@gmail.com>
2024-07-03 14:29:56 +0200

    logic to search for meals and restaurants added - returns right meals

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { auth } from '../../utils/firebase'
 
 const SearchResults = () => {
+    const user = auth.currentUser
+    const [result, setResult] = useState([])
+    const location = useLocation()
+    
+    useEffect(() => {
+        (
+            async() => {
+                const token = await user.getIdToken();
+
+                const params = new URLSearchParams(location.search)
+                const query = params.get('query')
+
+                const response = await axiosInstance.get(`/api/search?query=${query}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+
+                setResult(response.data)
+            }
+        )()
+    },[])
+
+    result.length > 0 ? console.log(result) : ""
   return (
     <div>SearchResults</div>
   )
