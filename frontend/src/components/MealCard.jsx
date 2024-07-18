0836b15fb7b959577448b14c4df9768db37e0e21
Raul <raul.ugia@gmail.com>
2024-07-18 15:02:29 +0200

    resposive styles added to Meal card

--- frontend/src/components/MealCard.jsx
+++ frontend/src/components/MealCard.jsx
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 
 const MealCard = ({id, mealName, restaurantName, thumbnailUrl}) => {
   return (
-    <Link to={`/my-meals/${id}`} className='shadow-md bg-slate-50 text-slate-800 w-[70%] h-32 mx-auto mt-4 py-4 px-4 flex items-center justify-between gap-5 rounded-lg'>
-        <div className='flex gap-10 justify-start'>
-            <div className='border h-24 w-24 rounded-md overflow-hidden'>
-                <img src={thumbnailUrl} alt="" className='h-full'/>
+    <Link to={`/my-meals/${id}`} className='shadow-md bg-slate-50 text-slate-800 w-[88%] md:w-[70%] h-32 mx-auto mt-4 py-4 px-4 flex items-center justify-between gap-5 rounded-lg'>
+        <div className='flex gap-5 md:gap-10 justify-start'>
+            <div className='min-h-24 h-24 w-24 min-w-24 rounded-md overflow-hidden border'>
+                <img src={thumbnailUrl} alt="" className='h-full w-full object-cover'/>
             </div>
-            <div className='flex flex-col mt-3'>
-                <p className='text-lg font-semibold'>{mealName}</p>
-                <p className='text-md text-gray-400'>{restaurantName}</p>
+            <div className='flex flex-col mt-3 gap-2'>
+                <p className='text-md md:text-lg font-semibold leading-[18px]'>{mealName}</p>
+                <p className='text-sm md:text-md text-gray-400 leading-[18px]'>{restaurantName}</p>
             </div>
         </div>
-        <MdKeyboardArrowRight size={40} />
+        <div>
+          <MdKeyboardArrowRight size={40} />
+        </div>
     </Link>
   )
 }
