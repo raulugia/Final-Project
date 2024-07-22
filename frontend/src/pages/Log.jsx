ae24f21099894806af5a8a8c5e19e84b1ab5b46c
Raul <raul.ugia@gmail.com>
2024-07-22 12:31:45 +0200

    Log - responsive styles

--- frontend/src/pages/Log.jsx
+++ frontend/src/pages/Log.jsx
@@ -57,34 +57,37 @@ const Log = () => {
             loading ? (
                 <div>Loading...</div>
             ) : (
-                <div className='border flex justify-start mt-8 bg-white md:w-[75%] max-w-[790px] rounded-lg px-8 py-7'>
-                    <div className='flex flex-row w-full'>
-                    <div className='w-[50%] max-w-[280px] overflow-hidden rounded-md'>
-                        <img src={log.picture} alt="" className='w-full h-full object-cover'/>
-                    </div>
-                    <div className='flex flex-col flex-grow ml-14 justify-between'>
-                        <div>
-                            <div className='flex flex-col items-start mb-5'>
-                                <h1 className='text-slate-800 md:text-2xl font-semibold'>{log.meal.name}</h1>
-                                <h3 className='text-slate-600 md:text-md'>{log.meal.restaurant.name}</h3>
+                <div className='border flex justify-start mt-8 bg-white w-[85%] md:w-[75%] max-w-[790px] rounded-lg px-8 py-7 shadow-md'>
+                    <div className='flex md:flex-row flex-col w-full'>
+
+                        <div className='md:w-[50%] max-w-[280px] overflow-hidden rounded-md mx-auto md:mx-0'>
+                            <img src={log.picture} alt="" className='w-full h-full object-cover'/>
+                        </div>
+
+                        <div className='flex flex-col flex-grow md:ml-14 mt-5 md:mt-0 justify-between'>
+                            <div>
+                                <div className='flex flex-col items-start mb-5'>
+                                    <h1 className='text-slate-800 md:text-2xl font-semibold'>{log.meal.name}</h1>
+                                    <h3 className='text-slate-600 md:text-md'>{log.meal.restaurant.name}</h3>
+                                </div>
+                                <div className='flex flex-col items-start'>
+                                    <p className='text-slate-700 font-semibold md:text-lg'>Carb estimate: <span className="font-normal">{log.carbEstimate}g</span></p>
+                                    <Accuracy accuracy={log.rating}/>
+                                    {
+                                        log.description && (
+                                            <div className='mt-5'>
+                                                <p className='text-slate-700 font-semibold md:text-lg'>Additional Information</p>
+                                                <p>{log.description}</p>
+                                            </div>
+                                        )
+                                    }
+                                </div>
                             </div>
-                            <div className='flex flex-col items-start'>
-                                <p className='text-slate-700 font-semibold md:text-lg'>Carb estimate: <span className="font-normal">{log.carbEstimate}g</span></p>
-                                <Accuracy accuracy={log.rating}/>
-                                {
-                                    log.description && (
-                                        <div className='mt-5'>
-                                            <p className='text-slate-700 font-semibold md:text-lg'>Additional Information</p>
-                                            <p>{log.description}</p>
-                                        </div>
-                                    )
-                                }
+                            <div className='ml-auto mt-8 md:mt-0'>
+                                <p className='text-sm text-slate-500'>Created {log.createdAt}</p>
                             </div>
                         </div>
-                        <div className='ml-auto'>
-                            <p className='text-sm text-slate-500'>Created {log.createdAt}</p>
-                        </div>
-                        </div>
+
                     </div>
                 </div>
             )
