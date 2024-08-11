5f6a55f4160ef7f985f41b0d8edb54746f020b6b
Raul <raul.ugia@gmail.com>
2024-08-11 14:01:33 +0200

    Account - modal created

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -30,6 +30,7 @@ const Account = () => {
     const [usernameAvailable, setUsernameAvailable] = useState()
     const [displayModal, setDisplayModal] = useState(true)
 
+
     useState(() => {
         (
             async() => {
@@ -318,15 +319,24 @@ const Account = () => {
         </div>
         {
             displayModal &&(
-                <div>
-                    <div>
-                        <div>
-                            <label htmlFor="email">Email</label>
-                            <input type="email"  id='email'/>
+                <div className='flex items-center justify-center absolute inset-0 z-100 w-full min-h-screen bg-black/60'>
+                    <div className='py-3 px-8 border rounded-lg shadow-md bg-white'>
+                        <div className="mb-4">
+                            <h3 className='text-2xl font-semibold'>Authentication Required</h3>
+                            <p className='text-sm'>Please enter your sing in details</p>
                         </div>
-                        <div>
-                            <label htmlFor="email">Email</label>
-                            <input type="password"  />
+                        <div className='flex flex-col gap-1 mb-3'>
+                            <label htmlFor="email" className='text-sm'>Email</label>
+                            <input 
+                                type="email"  id='email' className='py-1 px-2 rounded-md border'/>
+                        </div>
+                        <div className='flex flex-col gap-1 mb-2'>
+                            <label htmlFor="password" className='text-sm'>Password</label>
+                            <input type="password"  id='password' className='py-1 px-2 rounded-md border'/>
+                        </div>
+                        <div className='flex gap-3 w-full mb-2 mt-5'>
+                            <button type="button" className="py-1 px-2 rounded-md bg-gray-50 border mt-3 w-full">Cancel</button>
+                            <button type="button" className="py-1 px-2 rounded-md bg-blue-700 border border-blue-700 text-white mt-3 w-full">Confirm</button>
                         </div>
                     </div>
                 </div>
