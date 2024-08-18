3e40216469cdd7b41ea92331e2aba7dfedd48f16
Raul <raul.ugia@gmail.com>
2024-08-18 15:04:17 +0200

    skeleton home meal card updated

--- frontend/src/components/SkeletonHomeMealCard.jsx
+++ frontend/src/components/SkeletonHomeMealCard.jsx
@@ -3,71 +3,119 @@ import React from 'react'
 const SkeletonHomeMealCard = () => {
   return (
     <>
-    <div className='py-4 px-5 rounded-md border border-slate-200 shadow-md max-w-[456px] bg-white'>
+    <div className='flex flex-col overflow-hidden rounded-2xl min-h-[505px] border border-slate-200 shadow-md max-w-[456px] bg-white min-h-1/2'>
 
-        <div className='animate-pulse-fast min-w-full max-w-[415px] min-h-[370px] rounded-md overflow-hidden bg-slate-700'></div>
-        
-        <div className='mt-4'>
-            <div className='flex justify-between items-center'>
-                <div className='flex flex-col gap-2'>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-28'></div>
-                    <div className='animate-pulse-fast bg-slate-300  h-3 w-16'></div>
+        <div className='flex flex-col items-center bg-gradient-to-t from-sky-900 to-cyan-600 relative pt-2'>
+            <div className='flex flex-col gap-2 mb-3'>
+                <div className='animate-pulse-fast bg-slate-300  h-8 w-52'></div>
+                <div className='animate-pulse-fast bg-slate-300  h-4 w-24'></div>
+            </div>
+            <div className='w-[250px] min-h-[250px] rounded-3xl overflow-hidden bg-slate-700 z-10'></div>
+            <div className='bg-white w-[340px] h-[320px] absolute top-[130px] shadow-lg border rounded-3xl flex flex-col justify-end'>
+                <div className=' absolute z-10 bottom-0 flex justify-center w-full bottom-[-14px]'>
+                   <div className='border px-2 py-1 bg-slate-50 rounded-md w-28 h-8'>
+                        <div className='bg-slate-400 h-5 w-26 animate-pulse-fast rounded-sm'></div>
+                   </div>
                 </div>
-                <div>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-20'></div>
+                <div className='mb-5 ml-12'>
+                    <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
+                </div>
+                <div className='mb-10 mx-auto border'>
+                    <div className='animate-pulse-fast bg-slate-300  h-7 w-28'></div>
                 </div>
             </div>
         </div>
 
-        <div className='flex justify-end mt-2'>
-            <div className='animate-pulse-fast bg-slate-300  h-3 w-36'></div>
+        <div className='mt-4'>
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col gap-2'>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+        <div className='flex justify-end mt-auto'>
+            <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
+        </div>
+
+    </div>
+    <div className='flex flex-col overflow-hidden rounded-2xl min-h-[505px] border border-slate-200 shadow-md max-w-[456px] bg-white min-h-1/2'>
+
+        <div className='flex flex-col items-center bg-gradient-to-t from-sky-900 to-cyan-600 relative pt-2'>
+            <div className='flex flex-col gap-2 mb-3'>
+                <div className='animate-pulse-fast bg-slate-300  h-8 w-52'></div>
+                <div className='animate-pulse-fast bg-slate-300  h-4 w-24'></div>
+            </div>
+            <div className='w-[250px] min-h-[250px] rounded-3xl overflow-hidden bg-slate-700 z-10'></div>
+            <div className='bg-white w-[340px] h-[320px] absolute top-[130px] shadow-lg border rounded-3xl flex flex-col justify-end'>
+                <div className=' absolute z-10 bottom-0 flex justify-center w-full bottom-[-14px]'>
+                   <div className='border px-2 py-1 bg-slate-50 rounded-md w-28 h-8'>
+                        <div className='bg-slate-400 h-5 w-26 animate-pulse-fast rounded-sm'></div>
+                   </div>
+                </div>
+                <div className='mb-5 ml-12'>
+                    <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
+                </div>
+                <div className='mb-10 mx-auto border'>
+                    <div className='animate-pulse-fast bg-slate-300  h-7 w-28'></div>
+                </div>
+            </div>
+        </div>
+
+        <div className='mt-4'>
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col gap-2'>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+        <div className='flex justify-end mt-auto'>
+            <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
+        </div>
+
+    </div>
+    <div className='flex flex-col overflow-hidden rounded-2xl min-h-[505px] border border-slate-200 shadow-md max-w-[456px] bg-white min-h-1/2'>
+
+        <div className='flex flex-col items-center bg-gradient-to-t from-sky-900 to-cyan-600 relative pt-2'>
+            <div className='flex flex-col gap-2 mb-3'>
+                <div className='animate-pulse-fast bg-slate-300  h-8 w-52'></div>
+                <div className='animate-pulse-fast bg-slate-300  h-4 w-24'></div>
+            </div>
+            <div className='w-[250px] min-h-[250px] rounded-3xl overflow-hidden bg-slate-700 z-10'></div>
+            <div className='bg-white w-[340px] h-[320px] absolute top-[130px] shadow-lg border rounded-3xl flex flex-col justify-end'>
+                <div className=' absolute z-10 bottom-0 flex justify-center w-full bottom-[-14px]'>
+                   <div className='border px-2 py-1 bg-slate-50 rounded-md w-28 h-8'>
+                        <div className='bg-slate-400 h-5 w-26 animate-pulse-fast rounded-sm'></div>
+                   </div>
+                </div>
+                <div className='mb-5 ml-12'>
+                    <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
+                </div>
+                <div className='mb-10 mx-auto border'>
+                    <div className='animate-pulse-fast bg-slate-300  h-7 w-28'></div>
+                </div>
+            </div>
+        </div>
+
+        <div className='mt-4'>
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col gap-2'>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+        <div className='flex justify-end mt-auto'>
+            <div className='animate-pulse-fast bg-slate-300 mb-3 mr-5  h-3 w-36'></div>
         </div>
 
     </div>
     
-    <div className='py-4 px-5 rounded-md border border-slate-200 shadow-md max-w-[456px] bg-white'>
-
-        <div className='animate-pulse-fast min-w-full max-w-[415px] min-h-[370px] rounded-md overflow-hidden bg-slate-700'></div>
-        
-        <div className='mt-4'>
-            <div className='flex justify-between items-center'>
-                <div className='flex flex-col gap-2'>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-28'></div>
-                    <div className='animate-pulse-fast bg-slate-300  h-3 w-16'></div>
-                </div>
-                <div>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-20'></div>
-                </div>
-            </div>
-        </div>
-
-        <div className='flex justify-end mt-2'>
-            <div className='animate-pulse-fast bg-slate-300  h-3 w-36'></div>
-        </div>
-
-    </div>
-
-    <div className='py-4 px-5 rounded-md border border-slate-200 shadow-md max-w-[456px] bg-white'>
-
-        <div className='animate-pulse-fast min-w-full max-w-[415px] min-h-[370px] rounded-md overflow-hidden bg-slate-700'></div>
-        
-        <div className='mt-4'>
-            <div className='flex justify-between items-center'>
-                <div className='flex flex-col gap-2'>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-28'></div>
-                    <div className='animate-pulse-fast bg-slate-300  h-3 w-16'></div>
-                </div>
-                <div>
-                    <div className='animate-pulse-fast bg-slate-300  h-5 w-20'></div>
-                </div>
-            </div>
-        </div>
-
-        <div className='flex justify-end mt-2'>
-            <div className='animate-pulse-fast bg-slate-300  h-3 w-36'></div>
-        </div>
-
-    </div>
+    
     </>
   )
 }
