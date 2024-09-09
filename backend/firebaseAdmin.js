a75496a257a481e56fca6a39f33cc7cddb7e3e39
Raul <raul.ugia@gmail.com>
2024-09-09 11:01:39 +0200

    commit with zipped file

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,6 @@
 //import the Firebase Admin SDK
 const admin = require("firebase-admin");
-//import the service account key -
+//import the service account key - UNZIP FILE IN /backend/config COPY THE FILE NAME, EXTENSION INCLUDED, AND PASTE IT AFTER "./config/"
 const serviceAccount = require("./config/PASTE FILE NAME HERE.json")
 
 //initialize the Firebase Admin SDK
